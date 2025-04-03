a993c6d2dc1267a82f8880907eb4212c0f566388
Hanna <hana_mm@op.pl>
2025-04-03 20:38:21 +0200

    wyzwanie 4 zrobione

--- frontend/Lista6/wyzwanie.js
+++ frontend/Lista6/wyzwanie.js
@@ -95,3 +95,86 @@ console.timeEnd("idGenerator");
 //------------------------------------------------------------------
 
 // WYZWANIE 4
+
+function compareObjects(a, b) {
+  var aKeys = Object.keys(a); // zapisuje klucze obiektu a jako tablice
+  var bKeys = Object.keys(b);
+
+  if (aKeys.length !== bKeys.length) return false;
+
+  for (var prop of aKeys) {
+    if (!b.hasOwnProperty(prop)) return false; // sprawdza czy obiekt b ma dany klucz
+
+    if (typeof a[prop] === "object" && a[prop] !== null && b[prop] !== null) { 
+      if (!compareObjects(a[prop], b[prop])) return false;
+    } else {
+      if (a[prop] !== b[prop]) return false;
+    }
+  }
+  return true;
+}
+
+const obj1 = {
+  name: "Alice",
+  age: 25,
+  address: {
+    city: "Wonderland",
+    country: "Fantasy",
+  },
+};
+
+const obj2 = {
+  name: "Alice",
+  age: 25,
+  address: {
+    city: "Wonderland",
+    country: "Fantasy",
+  },
+};
+
+const obj3 = {
+  age: 25,
+  address: {
+    city: "Wonderland",
+    country: "Fantasy",
+  },
+  name: "Alice",
+};
+
+const obj4 = {
+  name: "Alice",
+  age: 25,
+  address: {
+    city: "Not Wonderland",
+    country: "Fantasy",
+  },
+};
+
+const obj5 = {
+  name: "Alice",
+};
+
+console.log("Should be True:", compareObjects(obj1, obj2));
+console.log("Should be True:", compareObjects(obj1, obj3));
+console.log("Should be False:", compareObjects(obj1, obj4));
+console.log("Should be True:", compareObjects(obj2, obj3));
+console.log("Should be False:", compareObjects(obj2, obj4));
+console.log("Should be False:", compareObjects(obj3, obj4));
+console.log("Should be False:", compareObjects(obj1, obj5));
+console.log("Should be False:", compareObjects(obj5, obj1));
+
+// ------------------------------------------------------------------
+
+// WYZWANIE 5
+
+let library = [];
+
+const addBookToLibrary = (title, author, pages, isAvailable, ratings) => {
+  library.push({
+    title,
+    author,
+    pages,
+    available: isAvailable,
+    ratings,
+  });
+};
